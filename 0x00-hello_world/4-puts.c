9db0834621187371185a68fd9deef69b15240e1c
MounaBenAli <benalimouna1986@gmail.com>
2021-09-30 10:09:02 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 
 int main()
 {
-   char string[40];
-   strcpy(str, "Programming is like building a multilingual puzzle\n");
-   puts(string);
+  
+   puts("Programming is like building a multilingual puzzle\n");
    return 0;
 }
