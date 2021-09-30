84f1488f8f50595974ec862161b36a005f164f07
MounaBenAli <benalimouna1986@gmail.com>
2021-09-30 10:31:29 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints a text
- *
- *Return: Always 0 (Success)
- */
-int main (void)
+*main - Prints a text
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle\n");
-   return 0;
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0i);
 }
