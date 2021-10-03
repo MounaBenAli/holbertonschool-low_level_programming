ba2bb2f74b8c0f4cc483a63ef5240b3c01443f7b
MounaBenAli <benalimouna1986@gmail.com>
2021-10-03 03:04:47 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Program that prints the alphabet in lowercas, and then in uppercase
+*
+* Return: Always(Success)
+*/
+int  main(void)
+{
+char ( ch,CH );
+for (ch = 'a'; ch = 'A'; ch <= 'z'; ch <= 'Z'; ch++; CH++)
+putchar(ch);
+putchar(CH);
+putchar ('\n');
+return (0);
+}
