1090e6bf2eb877b80c03cf05c147fe7f0a552a22
MounaBenAli <benalimouna1986@gmail.com>
2021-10-03 03:43:01 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main _ Program that prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always(Success)
+ */
+int main(void)
+int n;
+{
+printf("0123456789\n", n);
+return (0);
+}
