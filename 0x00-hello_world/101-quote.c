e3d210ac64c64a892c0507decd945031e8afe7fe
MounaBenAli <benalimouna1986@gmail.com>
2021-10-04 05:55:54 -0700

    Task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+*main - Prints string with Unix builtin writ()
+*
+*Return: standard error 
+*/
+int main(void)
+{
+write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",58);
+write(STDERR_FILENO,"/n",1);
+return (1);
+}
