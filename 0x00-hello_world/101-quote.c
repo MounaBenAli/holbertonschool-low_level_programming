6026537b56405f594d57229c0f4a1ea92ff30866
MounaBenAli <benalimouna1986@gmail.com>
2021-10-04 10:06:12 -0700

    Task 9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <unistd.h>
 /**
+*main - Prints string with Unix builtin write()
 *
-*main - Prints string with Unix builtin writ()
-*
-*Return-1
+*Return: Always 1(Success)
 */
 int main(void)
 {
 write
-(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
+(STDOUT_FILENO,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
 write(STDERR_FILENO, "\n", 1);
 return (1);
 }
