612ee4b224a2aeb1d23723b95910dda135ec02d1
MounaBenAli <benalimouna1986@gmail.com>
2021-10-05 03:50:22 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+*main - Prints _putchar followed by new line
+*
+* Return: Always(Success)
+*/
+int main(void)
+{
+char;
+_putchar(_putchar);
+putchar ('\n');
+return (0);
+}
