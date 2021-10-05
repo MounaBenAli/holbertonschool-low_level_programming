5fed8277673845bfc4ef34a356f50ceda3c2ab23
MounaBenAli <benalimouna1986@gmail.com>
2021-10-05 04:45:44 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
-*main - prints the alphabet in lowercase
+*main - prints"_putchar" in lowercase
 *
 * Return: Always(Success)
 */
 int  main(void)
 {
-char;
-_putchar("/_putchar");
+_putchar("_");
+_putchar ('\n');
+_putchar("p");
+_putchar ('\n');
+_putchar ('\n');
+_putchar ('\n');
 _putchar ('\n');
 return (0);
 }
