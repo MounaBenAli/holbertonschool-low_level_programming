9ee8d3ef36d3a0c3e5bede2554249215c06bc21d
MounaBenAli <benalimouna1986@gmail.com>
2021-10-05 04:09:25 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*main - Program that prints the alphabet in lowercase
+*
+* Return: Always(Success)
+*/
+void print_alphabet(void);
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar ('\n');
+return (0);
+}
