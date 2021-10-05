5a22fef4b38495318cd2323d75062d8a45f29056
MounaBenAli <benalimouna1986@gmail.com>
2021-10-05 12:08:47 -0700

    Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,20 +4,23 @@
  * @n: The number to be checked.
  *
  * Return: 1 and prints + if n > 0
- * Return: 0 and prints 0 if n equals 0
- * Return: -1 and prints - if n < 0
+ * Return1: 0 and prints 0 if n equals 0
+ * Return2: -1 and prints - if n < 0
  */
 int print_sign(int n)
 {
 if (n > 0)
+{
 _putchar('+');
 return (1);
+}
+else if (n < 0)
 {
-else (n < 0)
 _putchar('-');
 return (-1);
 }
-else ( n == 0 )
+else
+{
 _putchar('0');
 return (0);
 }
