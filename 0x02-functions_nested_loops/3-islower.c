9326658c038c1e6ff92106d02ed00fa600f509f2
MounaBenAli <benalimouna1986@gmail.com>
2021-10-05 10:15:28 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*islower_Write a function that checks for lowercase character
+*
+* Return is 1 if c is lowercase, otherwise return 0
+*/
+int _islower(int c)
+int = c;
+char ch;
+{	
+if (c = 'a'; c <= 'z'; c++)
+getchar(c);
+_putchar(c);
+_putchar('\n')
+return (1);
+{
+else if (c != 'a'; c >= 'z'; c++)
+_putchar(c)
+_putchar('\n')
+return (0);
+}
+}
