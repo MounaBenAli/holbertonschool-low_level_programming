30ffc9e2e65a68ef55b041c1855a12840b4cc8d2
MounaBenAli <benalimouna1986@gmail.com>
2021-10-07 01:06:03 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * add - Adds two integers.
+ * @a: The first integer to be added.
+ * @b: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
