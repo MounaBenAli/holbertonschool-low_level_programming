bd57b754c40e1dbff9a2adaff83c6d18178500e5
MounaBenAli <benalimouna1986@gmail.com>
2021-10-08 12:33:58 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,22 +7,22 @@
   */
 void print_square(int size)
 {
-	int i,j;
+int i,j;
 if (size < 0)
 {	
-	_putchar('\n');
+_putchar('\n');
 }
 else 
 {
-	for (i = 1; i <= size; i++)
-	{
-		for  (j = 1; j <= size; j++)
-		{
-		_putchar('#');
-		}
-		_putchar('\n');
-		}
-	}
+for (i = 1; i <= size; i++)
+{
+for  (j = 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
 
 		
