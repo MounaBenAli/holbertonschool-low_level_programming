1ec8206616477a4bafed25959c3ca050c654538f
MounaBenAli <benalimouna1986@gmail.com>
2021-10-08 04:55:38 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,34 @@
 #include "main.h"
 /**
-* jack_bauer - print every minute of the day starting from 00:00 to 23:59.
-*
-*not return
-*/
+ * jack_bauer - print every minute of the day starting from 00:00 to 23:59.
+ *
+ *not return
+ */
 void jack_bauer(void)
-int a, b, i, j, l;
 {
-l='9'
-for (a = '0'; a <= '2'; a++)
-{
-if (a == 2 && l == 3)
-{
-for (b = '0'; b <= '9'; b++)
-{
-for (i = '0'; i <= '5'; i++)
-{
-for (j = '0'; j <= '9'; j++)
-_putchar(a);
-_putchar(b);
-_putchar(':');
-_putchar(i);
-_putchar(j);
-}
-}
-}
-}
+
+	int a, b, i, j, l;
+
+	l='9';
+	for (a = '0'; a <= '2'; a++)
+	{
+		if (a == '2')
+		l = '3';
+		{
+			for (b = '0'; b <= l; b++)
+			{
+				for (i = '0'; i <= '5'; i++)
+				{
+					for (j = '0'; j <= '9'; j++)
+					{_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(i);
+					_putchar(j);
+					_putchar('\n');
+					}
+				}
+			}
+		}
+	}
 }
