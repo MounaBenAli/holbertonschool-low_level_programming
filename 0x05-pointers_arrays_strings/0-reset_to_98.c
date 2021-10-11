96d6d6c0b5a30e19ac2ef2ff891e58c239136eed
MounaBenAli <benalimouna1986@gmail.com>
2021-10-11 06:48:19 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *reset_to_98: takes a pointer to an int as parameter,updates the value to 98
+ *reset_to_98 - updates the value t
  *
  *@n: int to be updated
  *Return: empty
