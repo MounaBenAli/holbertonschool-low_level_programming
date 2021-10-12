cf7d75a72f40ce3ef854aa164daf095437e2a9f7
MounaBenAli <benalimouna1986@gmail.com>
2021-10-12 07:33:59 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,21 +1,22 @@
 #include "main.h"
-#include <string.h>
+
 /**
-  * puts_half- Prints second half of string to the stdout
-  *
-  * @str: The string to print
-  * @n: the last charchters of the string
-  * Return: empty
-  */
+* puts_half- Prints second half of string to the stdout
+*
+* @str: The string to print
+* @n: the last charachters of the string
+* Return: empty
+*/
 void puts_half(char *str)
 {
 int len, i, n;
 len = strlen(str);
 n = len / 2;
+if ((len % 2) == 1)
+n++;
 for (i = n; i < len; i++)
 {
 _putchar(str[i]);
-_putchar(n);
 }
 _putchar('\n');
 }
