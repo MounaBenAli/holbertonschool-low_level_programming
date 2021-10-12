8153dce2a48b19e291123455911cd2d526bbd94c
MounaBenAli <benalimouna1986@gmail.com>
2021-10-12 01:13:41 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,9 +8,9 @@
   */
 void print_rev(char *s)
 {
-int len;
-for (len = 0; *s != '\0'; s++)
-len++;
-while (len >= 0)
-_putchar(*s--), len--;
+int i, c = 0;
+for (i = 0; s[i] !='\0'; i++)
+c++;
+for (i = c -1; i >= 0; i --)
+_putchar(s[i]);
 }
