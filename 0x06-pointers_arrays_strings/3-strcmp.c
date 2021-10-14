9e55e0d946796c8013d5dd889002601debf005c0
MounaBenAli <benalimouna1986@gmail.com>
2021-10-14 10:36:23 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+  * _strcmp - Compares 2 strings
+  ** @s1: First String
+  * @s2 : Second String
+  * 
+  * Return:0 if s1[i]=s2[i]
+  */
+int _strcmp(char *s1, char *s2)
+{
+int i;
+
+
+for (i = 0 ; s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0'; i++)
+{}
+
+return (s1[i]-s2[i]);
+}
+
