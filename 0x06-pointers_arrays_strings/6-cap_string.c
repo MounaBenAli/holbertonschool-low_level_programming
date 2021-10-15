8dc60a6b93dbbea6a7733cb71442cef2142a90cc
MounaBenAli <benalimouna1986@gmail.com>
2021-10-15 08:21:59 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,35 +7,29 @@
  */
 char *cap_string(char *str)
 {
-	int i = 0;
-
-	if (str[0] >= 97 && str[i] <= 122)
-		str[0] = (str[0] - 32);
-
-	while (str[i])
-	{
-		if ((str[i] == ',') || (str[i] == ';') ||
-		(str[i] == '!') || (str[i] == '.') ||
-		(str[i] == '?') || (str[i] == '"') ||
-		(str[i] == '(') || (str[i] == ')') ||
-		(str[i] == '{') || (str[i] == '}') ||
-		(str[i] == '\n') || (str[i] == ' '))
-		{
-			i++;
-			if (str[i] >= 97 && str[i] <= 122)
-				str[i] = (str[i] - 32);
-			i--;
-		}
-
-		else if (str[i] == '\t')
-		{
-			str[i] = (str[i] + 23);
-			i++;
-			if (str[i] >= 97 && str[i] <= 122)
-				str[i] = (str[i] - 32);
-			i--;
-		}
-		i++;
-	}
-	return (str);
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0)
+{
+if ((str[i] >= 'a' && str[i] <= 'z'))
+str[i] = str[i] - 32;
+continue;
+}
+if (str[i] == ' ')
+{
+++i;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+continue;
+}
+}
+else
+{
+if (str[i] >= 'A' && str[i] <= 'Z')
+str[i] = str[i] + 32;
+}
+}
+return (str);
 }
