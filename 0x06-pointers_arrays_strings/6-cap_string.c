90624eed44b76dea250667b16f3a5d7d9194cb89
MounaBenAli <benalimouna1986@gmail.com>
2021-10-15 04:05:30 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+char *cap_string(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (i == 0)
+{
+if ((str[i] >= 'a' && str[i] <= 'z'))
+str[i] = str[i] - 32;
+continue;
+}
+if (str[i] == ' ')
+{
+++i;
+if (str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] = str[i] - 32;
+continue;
+}
+}
+else
+{
+if (str[i] >= 'A' && str[i] <= 'Z')
+str[i] = str[i] + 32;
+}
+}
+return (str);
+}
