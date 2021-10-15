f2db7b7f77e2826b3c89baf96ff9bc397a4fc2e7
MounaBenAli <benalimouna1986@gmail.com>
2021-10-15 08:04:24 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,34 +7,35 @@
  */
 char *cap_string(char *str)
 {
-int i = 0;
-for (i = 0; str[0] >= 97 && str[0] <= 122; i++)
-{
-str[0] = (str[0] - 32);
-}
-while (str[i])
-{
-if ((str[i] == ',') || (str[i] == ';') ||
-(str[i] == '!') || (str[i] == '.') ||
-(str[i] == '?') || (str[i] == '"') ||
-(str[i] == '(') || (str[i] == ')') ||
-(str[i] == '{') || (str[i] == '}') ||
-(str[i] == '\n') || (str[i] == ' '))
-{
-i++;
-if (str[i] >= 97 && str[i] <= 122)
-str[i] = (str[i] - 32);
-i--;
-}
-else if (str[i] == '\t')
-{
-str[i] = (str[i] + 23);
-i++;
-if (str[i] >= 97 && str[i] <= 122)
-str[i] = (str[i] - 32);
-i--;
-}
-i++;
-}
-return (str);
+	int i = 0;
+
+	if (str[0] >= 97 && str[i] <= 122)
+		str[0] = (str[0] - 32);
+
+	while (str[i])
+	{
+		if ((str[i] == ',') || (str[i] == ';') ||
+		(str[i] == '!') || (str[i] == '.') ||
+		(str[i] == '?') || (str[i] == '"') ||
+		(str[i] == '(') || (str[i] == ')') ||
+		(str[i] == '{') || (str[i] == '}') ||
+		(str[i] == '\n') || (str[i] == ' '))
+		{
+			i++;
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = (str[i] - 32);
+			i--;
+		}
+
+		else if (str[i] == '\t')
+		{
+			str[i] = (str[i] + 23);
+			i++;
+			if (str[i] >= 97 && str[i] <= 122)
+				str[i] = (str[i] - 32);
+			i--;
+		}
+		i++;
+	}
+	return (str);
 }
