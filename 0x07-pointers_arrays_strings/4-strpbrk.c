c5c12fc51aa9bc883ba1c8aa645f290c391a84f3
MounaBenAli <benalimouna1986@gmail.com>
2021-10-19 09:31:16 -0700

    Task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk-locates the first occurence in the s of any of the bytes of accept.
+ * @s: source string
+ * @accept: accepted string
+ * Return: a pointer *ptr to the byte in s that matches accept or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (s + i);
+}
+}
+}
+return (0);
+}
