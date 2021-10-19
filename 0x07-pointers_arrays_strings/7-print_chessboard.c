60be5a9b88b85b5c01c56be0cbb2afa486dac894
MounaBenAli <benalimouna1986@gmail.com>
2021-10-19 11:28:11 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,16 +7,13 @@
   */
 void print_chessboard(char (*a)[8])
 {
-	int b, c;
-
-	for (b = 0; b < 8; b++)
-	{
-		for (c = 0; c < 8; c++)
-		{
-			_putchar(a[b][c]);
-		}
-
-		_putchar('\n');
-	}
+int i, j;
+for (i = 0; i < 8 ; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
 }
-
+_putchar ('\n');
+}
+}
