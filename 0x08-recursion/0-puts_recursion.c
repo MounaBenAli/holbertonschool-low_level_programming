57ba547547b8329117accfd201b0d85f04b249ba
MounaBenAli <benalimouna1986@gmail.com>
2021-10-20 08:17:33 -0700

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/*
+* _puts_recursion:prints a string, followed by a new line
+*@s: string to be printed
+*
+*return:a nonnegative number on success, or EOF on  error.
+*/
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+return;
+_putchar(*s);
+_puts_recursion(s + 1);
+}
