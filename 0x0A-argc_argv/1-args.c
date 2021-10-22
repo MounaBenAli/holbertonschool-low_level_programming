8e1d483584bdaf97e6210e954742b876f7c1c50b
MounaBenAli <benalimouna1986@gmail.com>
2021-10-22 07:10:25 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Prints the number of arguments passed into it.
+ * @argc: number of command line arguments
+ * @argv: unused parameter
+ * Return: Always 0.
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+printf("%d\n", argc - 1);
+return (0);
+}
