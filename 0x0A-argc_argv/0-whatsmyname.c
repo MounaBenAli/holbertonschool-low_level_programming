3dcab070f05703464919cd95ff078db1416deaa0
MounaBenAli <benalimouna1986@gmail.com>
2021-10-22 04:26:03 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - Prints its name, followed by a new line.
+ * @argc: unused parameter
+ * @argv: name of the file
+ * Return: Always 0.
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+printf("%s", argv[0]);
+return (0);
+}
