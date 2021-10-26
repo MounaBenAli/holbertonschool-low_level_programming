ea8c14580574df3acf617b84008ff7168d7f74da
MounaBenAli <benalimouna1986@gmail.com>
2021-10-26 13:38:13 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -6,6 +6,7 @@
   * *str_concat- concatenates two strings
   * @s1: destination string
   * @s2: source string
+  * new: pointer appending s1 and s2
   * Return: pointer to the newly allocated space in memory of s1 and s2
 **/
 char *str_concat(char *s1, char *s2)
@@ -16,8 +17,11 @@ int s2_length = strlen(s2);
 int size = s1_length + s2_length + 1;
 char *new;
 
-if (s1 == NULL || s2 == NULL)
-return (NULL);
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
 
 new = malloc(sizeof(char) * size);
 if (!new)
@@ -27,7 +31,7 @@ for (; i < s1_length; i++)
 {
 new[i] = s1[i];
 }
-for (; j < s2_length; j++)
+for (; i < s2_length; j++)
 {
 new[s1_length + j] = s2[j];
 new[size - 1] = '\0';
