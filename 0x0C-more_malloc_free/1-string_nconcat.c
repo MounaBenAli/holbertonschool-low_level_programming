d0fed976f9208d81ff7ab5706f9f6531d348f537
MounaBenAli <benalimouna1986@gmail.com>
2021-10-28 10:45:31 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,38 +10,41 @@
   * @n: the specified number of characters in the souce string
   * Return: pointer to the concatenated string
   **/
-/**
- * Write a function that concatenates two strings.
-
-The returned pointer shall point to a newly allocated space in memory, which contains s1, followed by the first n bytes of s2, and null terminated
-If the function fails, it should return NULL
-If n is greater or equal to the length of s2 then use the entire string s2
-if NULL is passed, treat it as an empty string
-*/
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0;
-	unsigned int s1_length , s2_length, size;
- 	char *concat;
+	unsigned int i = 0, j = 0, k = 0, size = 0;
+	char *new;
 
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
+	i = strlen(s1);
+	k = strlen(s2);
 	
-	s1_length = strlen(s1);
-	s2_length = strlen(s2);
-	size = s1_length + s2_length + 1;
+	if (n >= k)
+	size = i + k;
+	else
+	size = i + n;
 
-	if (n >= s2_length)
-	n = s2_length;
-	concat = calloc(n , (sizeof(unsigned int) * size));
-	
-		for (; i < s1_length; i++)
+	new = malloc(sizeof(char) * size + 1);
+	if (!new)
+	return (NULL);
+
+	k = 0;
+	while (j < size)
 	{
-		concat[i] = s1[i];
-	}
-	for (; j < n; j++)
+	if (j <= i)
+	new[j] = s1[j];
+
+	if (j >= i)
 	{
-	concat[s1_length+ j] = s2[j];
-	concat[i] = '\0';
+	new[j] = s2[k];
+	k++;
 	}
-return (concat);
+	j++;
+	}
+	new[j] = '\0';
+	return (new);
 }
