a662eeb6db67b0f6ef665a6513901fbb90df1654
MounaBenAli <benalimouna1986@gmail.com>
2021-10-28 13:50:04 -0700

    Task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,20 +12,24 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-char *new;
+	char *new;
 
-if (new_size == old_size)
-return (ptr);
+	if (new_size == old_size)
+	return (ptr);
 
-if (!ptr)
-new = malloc(new_size);
+	if (!ptr)
+	new = malloc(new_size);
 
-if (new_size == 0)
-{
-free(ptr);
-return (NULL);
+	if (!new)
+	return (NULL);
+
+	return (new);
+	if (new_size == 0 && !ptr)
+	{
+	free(ptr);
+	return (NULL);
+	}
+	
+	return (new);
 }
 
-free(ptr);
-return (new);
-}
