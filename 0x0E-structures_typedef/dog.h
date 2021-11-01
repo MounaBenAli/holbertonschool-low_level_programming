e8e5b0bee93c09020dcef77356e8fd51c9450d06
MounaBenAli <benalimouna1986@gmail.com>
2021-11-01 08:09:18 -0700

    Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,4 +11,10 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog_t
+{
+        char *name;
+        float age;
+        char *owner;
+}dog_t;
 #endif
