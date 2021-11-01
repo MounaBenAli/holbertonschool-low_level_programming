9dbb8975d006f4b58fd04db5fd83aceae3a6bfc3
MounaBenAli <benalimouna1986@gmail.com>
2021-11-01 08:20:56 -0700

    Header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+/**
+*struct dog -new structure type
+ *@name:char *
+ *@age:float
+ *@owner: char *
+ */
 struct dog
 {
 	char *name;
@@ -11,10 +17,16 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+/**
+ *struct dog_t - is a new name for the type struct dog.
+ *@name:char *
+ *@age:float
+ *@owner:char *
+ */
 typedef struct dog_t
 {
-        char *name;
-        float age;
-        char *owner;
-}dog_t;
+char *name;
+float age;
+char *owner;
+} dog_t;
 #endif
