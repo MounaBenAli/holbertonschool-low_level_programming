36051689a0aae789b8781d8608fe4a11fdae6b0a
MounaBenAli <benalimouna1986@gmail.com>
2021-11-02 11:13:16 -0700

    Task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -22,12 +22,12 @@ void print_dog(struct dog *d)
 	if (d->age < 0)
 	printf("Age:(nil)\n");
 	else
-	printf("Age: %f\n", d->age);
+	printf("Age: %.6f\n", d->age);
 
 	if (d->owner == NULL)
 	printf("Owner:(nil)\n");
 	else
-	printf("color: %s\n", d->owner);
+	printf("Owner: %s\n", d->owner);
 }
 
 
