ebe41b15b6b4acc1b44bf7cd2045e2f6f7a098be
MounaBenAli <benalimouna1986@gmail.com>
2021-11-12 05:33:11 -0800

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,11 +13,13 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	if (h->str == NULL)
-	printf("[0], nil\n");
+
 
 	while (h != NULL)
 	{
+	if (h->str == NULL)
+	printf("[0] nil\n");
+	else
 	printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
 	n++;
