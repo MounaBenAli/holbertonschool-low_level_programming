9a5894860ae18cf0be4565413bc1323c21e24f08
MounaBenAli <benalimouna1986@gmail.com>
2021-11-17 03:47:07 -0800

    Task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,14 +14,16 @@ void free_listint2(listint_t **head)
 {
 
 	listint_t *temp;
-temp = *head;
 
-	while (temp != NULL || temp->n < 0)
+	if (head != NULL && *head != NULL)
 	{
-	temp = temp->next;
-	free(*head);
-	*head = temp;
+		temp = *head;
+		while (temp != NULL)
+		{
+			temp = temp->next;
+			free(*head);
+			*head = temp;
+		}
 	}
 	*head = NULL;
-
 }
