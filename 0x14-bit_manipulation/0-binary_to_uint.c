0a9fbcc8405c9fd51becc12e639cc07625557899
MounaBenAli <benalimouna1986@gmail.com>
2021-11-19 04:43:37 -0800

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,8 +15,11 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] != '\0')
 	{
+	if (b[i] != '0' && b[i] != '1')
+	return (0);
+
 	val <<= 1;
 	val += b[i] - '0';
 	i++;
