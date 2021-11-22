3f9f22b5f29c1859d3e00adcda564b79ace4ae24
MounaBenAli <benalimouna1986@gmail.com>
2021-11-22 10:36:33 -0800

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,11 +10,11 @@
 int _strlen(char *s);
 
 /**
- * create_file - creates a file
+ * create_file-creates a file
  * @filename: text file to read and printed
  * @text_content: a NULL terminated string to write to the file
  *
- * Returns: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 
 
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (filename != NULL)
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC,0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -36,13 +36,11 @@ int create_file(const char *filename, char *text_content)
 		if (fd == -1)
 			return (-1);
 	}
-	
 	if (text_content != NULL)
 		write(fd, text_content, _strlen(text_content));
 
 	close(fd);
 	return (1);
-
 }
 
 
