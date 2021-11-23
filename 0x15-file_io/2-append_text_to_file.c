f2bc67da52518911782d5395360c2a05819321c9
MounaBenAli <benalimouna1986@gmail.com>
2021-11-23 01:45:35 -0800

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,28 +18,24 @@ int _strlen(char *s);
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
+
+	if (text_content != NULL)
+	{
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
-	if (filename != NULL)
-		return (1);
-	if (text_content != NULL)
-		write(fd, text_content, _strlen(text_content));
-	return (-1);
 
-	if (access(filename, F_OK) == 0)
-	{
-		return (1);
-	}
-	else
+	i = write(fd, text_content, _strlen(text_content));
+	if (i == -1)
 	{
 		return (-1);
 	}
-	close(fd);
+		close(fd);
+	}
 	return (1);
 }
 
