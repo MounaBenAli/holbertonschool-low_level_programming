14e657619bd91e295777c760adad72a5717de206
MounaBenAli <benalimouna1986@gmail.com>
2022-01-06 03:24:56 -0800

    "header"

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,10 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -16,5 +20,10 @@ int n;
 struct dlistint_s *prev;
 struct dlistint_s *next;
 } dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+
 
 #endif
