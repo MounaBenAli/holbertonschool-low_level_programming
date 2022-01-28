3837b74854229ef0fc646a02d22375b3c5820814
MounaBenAli <benalimouna1986@gmail.com>
2022-01-28 14:52:28 +0100

    "index of the key"

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives the index of the key
+ * @key : the key
+ * @size : size of array of hash table
+ *
+ * Return: the index at which the key/value pair
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
+
