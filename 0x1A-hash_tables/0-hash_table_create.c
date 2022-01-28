2c9a5cb9cffa647a7e36f5cd1d6620975c962216
MounaBenAli <benalimouna1986@gmail.com>
2022-01-28 16:04:53 +0100

    "fixing"

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,8 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-	hash_table_t *new_table;
+	hash_table_t *new_table = NULL;
+	hash_node_t **arr = NULL;
 
 	/**allocate table**/
 	new_table = malloc(sizeof(hash_table_t));
@@ -18,16 +19,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 		return (NULL);
 
-    new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t*) * size);
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (arr == NULL)
 
-	/**return NULL if mem allocation fails**/
-	if (new_table->array == NULL)
-		{
-			free(new_table);
-			return(NULL);
-		}
-		for (; i < size; ++i) 
-			new_table->array[i] = NULL;
+	return (NULL);
+
+		for (; i < size; ++i)
+			arr[i] = NULL;
+
+			   new_table->size = size;
+	new_table->array = arr;
 	return (new_table);
 }
