e24784c01815535ac738419a155c7ce8e08c17a9
MounaBenAli <benalimouna1986@gmail.com>
2022-01-28 11:55:24 +0100

    testing

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/*
+*hash_table_create: creates a hash table.
+*@size: is the size of the array
+Return: a pointer to the newly created hash table or NULL
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	/**allocate table**/
+	hash_table_t *newtable = malloc(sizeof(hash_table_t) * 1);
+	
+	/*if error in mem allocation return NULL*/
+	if(!newtable)
+	 {
+		 return(NULL);
+	 }
+
+	/**allocate table entries**/
+	newtable->array = malloc(sizeof(hash_node_t*) * size);
+
+	/**Set each to NULL needed for proper operation**/
+	for(; i < size; ++i)
+	{
+		newtable->array[i] = NULL;
+	}
+	/*return pointer to new hash table*/
+	return(newtable);
+}
