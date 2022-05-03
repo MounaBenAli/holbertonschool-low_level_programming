fcc1147b21f330a0672e2b80ae0066072cf4f4a3
MounaBenAli <benalimouna1986@gmail.com>
2022-05-03 13:37:11 +0200

    i dunno, maybe this works checker is unhappy :wink:

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,13 +11,19 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
+	{
 		if (array[i] == value)
-			return (i);
-	return (-1);
+		break;
+	}
+
+	if (i == size)
+		return (-1);
+
+	return (i);
 }
